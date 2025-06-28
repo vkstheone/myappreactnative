1b846c2f114c903aef72e9b603ef08b07a82100e
vkstheone <vks7pp@gmail.com>
2025-06-28 16:26:18 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import {View, Text, Image} from 'react-native'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(){
+  let nome = 'Vinicius';
+  let Img = 'https://blog.polipet.com.br/wp-content/uploads/2024/01/pato-1110x508.jpeg'
+  return(
+    <View>
+      <Text>Meu primeiro App</Text>
+      <Text style={{color: 'red', fontSize:30, margin: 70}}>Ola mundo meu nome e {nome} e</Text>
+      <Text style={{color: 'red', fontSize: 30, left: 30}}> Kennedy e meu pato</Text>
+      <Image source={{uri:Img}} style={{width: 300, height:300, left:50, margin: 5}} />
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
